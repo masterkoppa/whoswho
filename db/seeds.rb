6429c6cf15307c88b7342d08d5b9b63d3e085b37
Andres Ruiz <andresruiz2010@gmail.com>
2015-08-16 15:54:27 -0700

    Add better seeding for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,18 @@ puts 'CREATED ADMIN USER: ' << user.email
 # Environment variables (ENV['...']) can be set in the file config/application.yml.
 # See http://railsapps.github.io/rails-environment-variables.html
 
-[ ["John Smith", "test@gmail.com"], 
-  ["Test Dummy", "andresruiz2010@gmail.com"],
-  ["Testing 123", "jared.rit@gmail.com"], 
-  ["Well Shit", "lol@gmail.com"] ].each do |name, email|
+email_list = ["andresruiz2010@gmail.com", "test@gmail.com", "jared.rit@gmail.com", "lol@gmail.com"]
+name_list  = ["John Smith", "Test Dummy", "Testing 123", "Well Shit"]
+image_urls = ["https://masterkoppa.smugmug.com/Featured/i-b3BqvQ2/0/M/DSC_0841-M.jpg", 
+              "https://masterkoppa.smugmug.com/Featured/i-NGLSrfZ/0/L/DSC00449-L.jpg",
+              "https://masterkoppa.smugmug.com/Featured/i-JzB8Wwn/0/M/DSC_0522-M.jpg"]
 
-  Employee.create(email: email, full_name: name)
+# Create 1k Employees
+(0..1000).each do |number|
+    puts "Employee #{number}"
+    email = email_list.sample(1)[0]
+    name  = name_list.sample(1)[0]
+    image = rand(2) == 0 ? image_urls.sample(1)[0] : nil
+
+    Employee.create(email: email, full_name: name, url: image)
 end
